d88623067a6073e2b729af455f90679f1e2dff0d
Carol Silva Lopes <carolsilva2lopes@gmail.com>
2026-03-25 02:22:24 -0300

    fix csr

--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['ayehyxbqslusamntcyap.supabase.co'],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
-
-module.exports = nextConfig
+﻿const nextConfig = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true }, experimental: { missingSuspenseWithCSRBailout: false } }; module.exports = nextConfig
